11a09e1eb273014dec09ebced3cbdb459152fd09
miguel <miguel.jarecca@gmail.com>
2025-07-18 14:14:47 -0500

    correct product card

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { formatPrice } from "../utils/formatters/FormatPrice";
+import { formatPrice } from "../utils/formatters/formatPrice";
 
 export const ProductCard = ({ product, handlerAddCart }) => {
 
